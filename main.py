0d89bde0d58d19204712f1a15997f01d30ad83d5
SuhyeonKim <suhyeon0422@uzurotech.com>
2024-07-24 14:53:41 +0900

    test

--- main.py
+++ main.py
@@ -6,23 +6,18 @@ app = FastAPI()
 async def root():
     return {'message': 'Hello, World!'}
 
+@app.get('/msg')
+async def msg():
+    return {'message': 'yorokobiganai'}
+
+@app.post('/hello/{name}')
+async def hello(name: str):
+    
+    return {'message': f'Hello, {name}!'}
+
 def main():
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=8000)
 
 if __name__ == '__main__':
     main()
-
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/')
-async def root():
-    return {'message': 'yorokobiganai'}
-
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
